22fe8489e86d4b68a551337c1f8789912ec42a31
Ashok Sah <ashok.stelmec@gmail.com>
2022-08-06 23:13:26 +0530

    UseState & UseEffect & UseRef Hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect, useRef } from "react";
 
 function App() {
+  const counter = useRef(0);
+  const [number, setNumber] = useState(0);
+
+  useEffect(() => {
+    console.log(counter.current);
+    counter.current = counter.current + 1;
+  });
+
+  const handleClick = () => {
+    setNumber(number + 1);  
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{`This component is rendered ${counter.current} times`}</h1>
+      <button onClick={handleClick}> Increase Number</button>
+      <br /> <br />
+      {number}
     </div>
   );
 }
